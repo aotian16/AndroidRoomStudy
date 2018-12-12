f5bef2ebaa5d026fed290832d889d12a523ac487
tongjin <aotian16@gmail.com>
2018-12-12 20:41:13 +0800

    update

--- app/src/main/java/com/qefee/androidroomstudy/App.java
+++ app/src/main/java/com/qefee/androidroomstudy/App.java
@@ -1,7 +1,9 @@
 package com.qefee.androidroomstudy;
 
 import android.app.Application;
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Room;
+import android.arch.persistence.room.migration.Migration;
 
 import com.qefee.androidroomstudy.db.AppDatabase;
 
@@ -25,7 +27,29 @@ public class App extends Application {
         instance = this;
 
         initDb();
+
+        // 升级Room
+//        Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name")
+//                .addMigrations(MIGRATION_1_2, MIGRATION_2_3).build();
     }
+
+//    // 定义升级，从版本1升级到2
+//    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+//        @Override
+//        public void migrate(SupportSQLiteDatabase database) {
+//            database.execSQL("CREATE TABLE `Fruit` (`id` INTEGER, "
+//                    + "`name` TEXT, PRIMARY KEY(`id`))"); // 新建一个table
+//        }
+//    };
+//
+//    // 定义升级，从版本2升级到3
+//    static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+//        @Override
+//        public void migrate(SupportSQLiteDatabase database) {
+//            database.execSQL("ALTER TABLE Book "
+//                    + " ADD COLUMN pub_year INTEGER"); // 插入字段
+//        }
+//    };
 
     private void initDb() {
         // 这里初始化room
